de8516ede3ada99444ebd66493a99a3a5a897933
ayaanqui <ayaanqui.com@gmail.com>
2023-03-02 23:51:55 -0600

    refactor: format file

--- src/main.go
+++ src/main.go
@@ -14,30 +14,30 @@ func main() {
 	app := &cli.App{
 		Flags: []cli.Flag{
 			&cli.StringFlag{
-				Name: "directory",
-				Usage: "specifies the directory that results are stored",
+				Name:        "directory",
+				Usage:       "specifies the directory that results are stored",
 				Destination: &config.Directory,
-				Required: true,
+				Required:    true,
 			},
 		},
 		Commands: []cli.Command{
 			{
-				Name: "create-migration",
+				Name:      "create-migration",
 				ShortName: "c",
-				Usage: "creates a new migration file",
+				Usage:     "creates a new migration file",
 				Action: func(c *cli.Context) error {
 					migration_tool := tool.MigrationTool{
 						Config: &config,
 					}
 					migration_tool.Config.Directory = tool.StringTrailingSlash(migration_tool.Config.Directory)
-                    migration_name := c.Args().First()
+					migration_name := c.Args().First()
 					return migration_tool.CreateMigrationFile(migration_name)
-                },
+				},
 			},
 		},
-    }
+	}
 
-    if err := app.Run(os.Args); err != nil {
-        log.Fatal(err)
-    }
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
 }
